f15c9e5786a163777f438d575df24f36f17f7b33
Shin <ykingdev@gmail.com>
2023-07-06 21:03:26 +0200

    Error resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-red-500">{children}</body>
     </html>
   )
 }
